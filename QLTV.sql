742b04e614c14c53c83295a9ff5ed496e59161d1
PTD504 <22520193@gm.uit.edu.vn>
2024-05-04 11:37:01 +0700

    Update QLTV.sql file

--- QLTV.sql
+++ QLTV.sql
@@ -2,17 +2,20 @@
 GO
 USE QLTV_BETA
 GO
-SET DATEFORMAT DMY
+SET DATEFORMAT DMY -- Cần thống nhất định dạng của ngày tháng trong CSDL
 GO
 
 CREATE TABLE DOCGIA
 (
 	MaDG		VARCHAR(5) PRIMARY KEY,
 	LoaiDG		VARCHAR(255),
+	HoTen		VARCHAR(50) NOT NULL,
 	NgaySinh	DATE,
 	DiaChi		VARCHAR(255),
-	Email		VARCHAR(255),
+	Email		VARCHAR(255) NOT NULL,
+	GioiTinh	VARCHAR(3),
 	NgayLapThe	DATE,
+	SoDT		VARCHAR(10),
 	IsDeleted	BIT DEFAULT 0
 	/* Các loại độc giả
 	Độc giả sinh viên: Bao gồm các sinh viên đang học tại trường hoặc các cơ sở giáo dục. - LDOCGIA_A
@@ -27,14 +30,14 @@ GO
 CREATE TABLE SACH 
 (
 	MaSach		VARCHAR(5) PRIMARY KEY,
-	TenSach		VARCHAR(255),
-	TheLoai		VARCHAR(255),
-	TacGia		VARCHAR(255),
-	NamXB		SMALLINT,
-	NhaXB		VARCHAR(255),
-	NgayNhap	DATE,
-	TriGia		INT,
-	TinhTrang	BIT,
+	TenSach		VARCHAR(255) NOT NULL,
+	TheLoai		VARCHAR(255) NOT NULL,
+	TacGia		VARCHAR(255) NOT NULL,
+	NamXB		SMALLINT NOT NULL,
+	NhaXB		VARCHAR(255) NOT NULL,
+	NgayNhap	DATE NOT NULL,
+	TriGia		INT NOT NULL,
+	TinhTrang	BIT NOT NULL,
 	IsDeleted	BIT DEFAULT 0
 );
 GO
@@ -42,13 +45,15 @@ GO
 CREATE TABLE PHIEUMUON
 (
 	MaPhMuon	VARCHAR(5) NOT NULL PRIMARY KEY,
-	MaDG		VARCHAR(5) FOREIGN KEY REFERENCES DOCGIA(MaDG),
-	MaSach		VARCHAR(5) FOREIGN KEY REFERENCES SACH(MaSach),
-	NgayMuon	DATE,
-	NgayPhTra	DATE,
+	MaDG		VARCHAR(5) NOT NULL FOREIGN KEY REFERENCES DOCGIA(MaDG),
+	MaSach		VARCHAR(5) NOT NULL FOREIGN KEY REFERENCES SACH(MaSach),
+	NgayMuon	DATE NOT NULL,
+	NgayPhTra	DATE NOT NULL,
 	IsDeleted	BIT DEFAULT 0
 );
 GO
+
+SELECT * FROM PHIEUMUON
 
 CREATE TABLE PHIEUTRA
 (
@@ -93,20 +98,26 @@ GO
 
 CREATE TABLE PARAMETERs
 (
-	IDDocGia	INT 	DEFAULT 0,
-	IDSach		INT 	DEFAULT 0,
-	IDPhMuon	INT 	DEFAULT 0,
-	IDPhThu		INT 	DEFAULT 0,
-	IDPhTra		INT 	DEFAULT 5000,
-	SoNgMuonTD	INT 	DEFAULT 30
+	-- Cần thống nhất các giá trị ràng buộc cần thiết (Tuổi tối đa, tuổi tối thiểu, số ngày mượn tối đa, ...)
+	IDSearch	SMALLINT	DEFAULT 1, -- Biến này phục vụ trong quá trình cần lấy dữ liệu từ bảng PARAMETERS
+	IDDocGia	INT 		DEFAULT 0,
+	IDSach		INT 		DEFAULT 0,
+	IDPhMuon	INT 		DEFAULT 0,
+	IDPhThu		INT 		DEFAULT 0,
+	IDPhTra		INT 		DEFAULT 5000,
+	SoNgMuonTD	INT 		DEFAULT 30,
+	TuoiTT		SMALLINT	DEFAULT 15,
+	TuoiTD		SMALLINT	DEFAULT 60
 )
 GO
 
 -- INSERT DATA INTO PARAMETERS
-INSERT INTO PARAMETERs VALUES(DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO PARAMETERs VALUES(DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT);
 GO
 
--- Update IDDocGia when we insert a new row in table DOCGIA
+-- TẠO CÁC RÀNG BUỘC KHI THAO TÁC VỚI DỮ LIỆU TRONG BẢNG DOCGIA
+
+-- Cập nhật IDDocGia khi thêm một độc giả mới vào CSDL 
 CREATE TRIGGER INSERT_INTO_DOCGIA
 ON DOCGIA
 FOR INSERT
@@ -117,7 +128,49 @@ BEGIN
 END
 GO
 
--- Update
+-- Giới tính của độc giả phải thuộc (nam, nữ)
+ALTER TABLE DOCGIA ADD CONSTRAINT CHECK_GIOITINH CHECK(GioiTinh IN ('Nam', 'Nữ'));
+GO
+
+-- Tuổi của độc giả phải lớn hơn hoặc bằng độ tuổi tối thiểu (TuoiTT) và nhỏ hơn hoặc bằng độ tuổi tối đa trong quy định (TuoiTD)
+CREATE TRIGGER INSERT_UPDATE_TUOI_DOCGIA
+ON DOCGIA
+FOR INSERT, UPDATE
+AS
+BEGIN
+	DECLARE @NgaySinh DATE, @NgayLapThe DATE, @TuoiKC SMALLINT
+	DECLARE @TuoiInf SMALLINT, @TuoiSup SMALLINT
+
+	SELECT @NgaySinh = NgaySinh FROM INSERTED
+	SELECT @NgayLapThe = NgayLapThe FROM INSERTED
+	SELECT @TuoiInf = TuoiTT FROM PARAMETERs WHERE IDSearch = 1
+	SELECT @TuoiSup = TuoiTD FROM PARAMETERs WHERE IDSearch = 1
+	SELECT @TuoiKC = DATEDIFF(year, @NgayLapThe, @NgaySinh)
+
+	IF (@TuoiKC > @TuoiSup OR @TuoiKC < @TuoiInf) 
+	BEGIN
+		RAISERROR('Tuổi của độc giả phải nằm trong độ tuổi đã có trong quy định', 16, 1)
+		ROLLBACK TRAN
+		RETURN
+	END
+END
+GO
+
+-- Định dạng của email phải là: expamle@example.com
+ALTER TABLE DOCGIA ADD CONSTRAINT CHECK_EMAIL_PATTERN CHECK(Email LIKE ('%@%.com'));
+GO
+
+-- Số điện thoại phải đúng định dạng số điện thoại của Việt Nam
+ALTER TABLE DOCGIA ADD CONSTRAINT CHECK_SDT CHECK(LEN(SoDT) = 10 AND SUBSTRING(SoDT, 0, 3) IN ('032', '033', '034', '035', '036', '037', '038', '039', '096', '097', '098', '086', '083', '084', '085', '081', '082', '088', '091', '094', '070', '079', '077', '076', '078', '090', '093', '089', '056', '058', '092', '059', '099'));
+GO
+
+-- Ngày lập thẻ phải nhỏ hơn hoặc đúng bằng ngày hiện tại
+ALTER TABLE DOCGIA ADD CONSTRAINT CHECK_NGAYLT CHECK(NgayLapThe <= GETDATE());
+GO
+
+-- TẠO CÁC RÀNG BUỘC KHI THAO TÁC VỚI DỮ LIỆU TRONG BẢNG SACH
+
+-- Cập nhật IDSach khi thêm một cuốn sách mới vào CSDL
 CREATE TRIGGER INSERT_INTO_SACH
 ON SACH
 FOR INSERT
@@ -127,6 +180,20 @@ BEGIN
 	SET IDSach = IDSach + 1
 END
 GO
+
+-- Năm xuất bản phải bé hơn hoặc bằng năm nhập
+ALTER TABLE SACH ADD CONSTRAINT CHECK_NAMXB CHECK(NamXB <= YEAR(NgayNhap));
+GO
+
+-- Ngày nhập phải nhỏ hơn hoặc bằng ngày hiện tại
+ALTER TABLE SACH ADD CONSTRAINT CHECK_NAMNHAP CHECK(NgayNhap <= GETDATE());
+GO
+
+-- Trị giá phải đúng định dạng số và là số không âm
+ALTER TABLE SACH ADD CONSTRAINT CHECK_TRIGIA CHECK(TRIGIA >= 0);
+GO
+
+-- TẠO CÁC RÀNG BUỘC KHI THAO TÁC VỚI DỮ LIỆU TRONG BẢNG PHIEUMUON
 
 -- Tạo TRIGGER để tính ngày phải trả của cuốn sách
 CREATE TRIGGER NGAY_PHAI_TRA_PHIEUMUON
@@ -153,6 +220,7 @@ BEGIN
 END;
 GO
 
+-- TẠO CÁC RÀNG BUỘC KHI THAO TÁC VỚI DỮ LIỆU TRONG BẢNG PHIEUTRA
 -- Tạo TRIGGER kiểm tra có trả sách quá hạn không, nếu có thì tạo phiếu thu trong bảng PHIEUTHU tương ứng với phiếu mượn, phiếu trả đó
 CREATE TRIGGER KIEM_TRA_QUA_HAN_PHIEUTRA
 ON PHIEUTRA
