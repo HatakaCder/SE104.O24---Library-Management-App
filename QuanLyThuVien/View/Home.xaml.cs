42bc66b8d21b87370a22207e48be372f560e811c
Lường Đại Phát <21522443@gm.uit.edu.vn>
2024-05-11 20:54:00 +0700

    update datagrid

--- QuanLyThuVien/View/Home.xaml.cs
+++ QuanLyThuVien/View/Home.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QuanLyThuVien.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace QuanLyThuVien.View
         public Home()
         {
             InitializeComponent();
+            DataContext = new HomeVM();
         }
     }
 }
--- QuanLyThuVien/ViewModel/HomeVM.cs
+++ QuanLyThuVien/ViewModel/HomeVM.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using QuanLyThuVien.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace QuanLyThuVien.ViewModel
 {
-    class HomeVM
+    public class HomeVM 
     {
+        private List<DOCGIA> _readers;
+        public List<DOCGIA> DOCGIAs
+        {
+            get { return _readers; }
+            set { _readers = value; }
+        }
+        public HomeVM() {
+            LoadData(); 
+        }
+        public void LoadData()
+        {
+           QLTV_BETAEntities1 db = new QLTV_BETAEntities1();
+           DOCGIAs =  db.DOCGIAs.ToList();
+        }
     }
 }
