0664266768448190d833cbfbd494d87f9d86ac88
Phạm Việt <74823960+HatakaCder@users.noreply.github.com>
2024-06-11 16:06:20 +0700

    add Services Folder

---
+++ QuanLyThuVien/Services/ForgotPasswordService.cs
@@ -0,0 +1,55 @@
+﻿
+using QuanLyThuVien.Class;
+using QuanLyThuVien.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Media3D;
+
+namespace QuanLyThuVien.Services
+{
+    public class ForgotPasswordService
+    {
+        QLTV_BETAEntities ObjContext;
+        PasswordHashing ph;
+        public ForgotPasswordService()
+        {
+            ObjContext = new QLTV_BETAEntities();
+            ph = new PasswordHashing();
+        }
+        public string SendOtp(string email)
+        {
+            string otp;
+            try
+            {
+                OTP Otp = new OTP();
+                otp = Otp.GenerateOTP(6);
+                Otp.SendEmail(email, otp);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return otp;
+        }
+        public bool ChangePassword(string email, string password)
+        {
+            bool IsChanged = false;
+            try
+            {
+                var obj_dg = ObjContext.DOCGIAs.FirstOrDefault(x => x.Email == email);
+                var obj_acc = ObjContext.ACCOUNTs.FirstOrDefault(x => x.MaDG == obj_dg.MaDG);
+                obj_acc.MatKhau = ph.HashPassword(password);
+                var NoOfRowsAffected = ObjContext.SaveChanges();
+                IsChanged = NoOfRowsAffected > 0;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return IsChanged;
+        }
+    }
+}
