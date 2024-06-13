1d0a1054c3cf56e65d0a6f9030c310c26defb19a
PTD504 <22520193@gm.uit.edu.vn>
2024-06-13 18:49:20 +0700

    Update QLTV.sql

--- QLTV.sql
+++ QLTV.sql
@@ -1,51 +1,42 @@
-﻿/*
-Mọi người có thể thêm dữ liệu vào file CSDL_Excel.xlsx (mỗi người một ít, chủ yếu tự nghĩ ra á) rồi sau đó sẽ thêm vào file sql sau.
-Khi thêm dữ liệu thì mọi người lưu ý định dạng ngày tháng dd/mm/yyyy
-Và lưu ý các mối quan hệ khóa ngoại liên kết giữa các bảng. Thuộc tính nào chưa thể thêm vào thì mọi người cứ đặt là NULL.
-*/
-
-CREATE DATABASE QLTV_BETA
+﻿CREATE DATABASE QLTV_BETA
 GO
 USE QLTV_BETA
 GO
 SET DATEFORMAT DMY -- dateformat: dd/mm/yyyy
-GO
+GO﻿
 
 CREATE TABLE DOCGIA 
 (
 	MaDG		VARCHAR(5) NOT NULL PRIMARY KEY,
-	HoTen		VARCHAR(30),
-	LoaiDG		VARCHAR(255),
-	GioiTinh	VARCHAR(3),
+	HoTen		NVARCHAR(30),
+	GioiTinh	NVARCHAR(3),
 	NgaySinh	DATE,
-	DiaChi		VARCHAR(255),
+	DiaChi		NVARCHAR(255),
 	Email		VARCHAR(255) NOT NULL,
 	SoDT		VARCHAR(10) NOT NULL,
 	NgayLapThe	DATE,
-	SoCCCD		VARCHAR(12) NOT NULL,
-	--AnhDaiDien	VARCHAR(30), -- Nếu thấy không cần thì có thể bỏ qua thuộc tính này (thuộc tính này sẽ lưu đường dẫn ảnh đại diện của người dùng đặt trong thư mục Debug, khi nào xem thông tin cụ thể của một người dùng thì mình lấy ra)
 	IsDeleted	BIT DEFAULT 0
-	/* Các loại độc giả
-	Độc giả sinh viên: Bao gồm các sinh viên đang học tại trường hoặc các cơ sở giáo dục. - LDOCGIA_A
-	Độc giả học sinh: Bao gồm học sinh đang theo học tại các trường cấp 1, 2, 3. - LDOCGIA_B
-	Cán bộ giáo viên: Bao gồm giáo viên, nhân viên trong ngành giáo dục. - LDOCGIA_C
-	Người đã đi làm: Độc giả không thuộc các nhóm trên, đã đi làm và có nhu cầu sử dụng thư viện. - LDOCGIA_D
-	Các đối tượng khác: Có thể bao gồm người nước ngoài, người nghiên cứu, người quan tâm đến tài liệu thư viện. - LDOCGIA_E
-	*/
+);
+GO
+
+CREATE TABLE THELOAI
+(
+	TenTheLoai	NVARCHAR(225) PRIMARY KEY,
+	IsDeleted	BIT DEFAULT 0
 );
 GO
 
 CREATE TABLE SACH 
 (
 	MaSach		VARCHAR(5) NOT NULL PRIMARY KEY,
-	TenSach		VARCHAR(255),
-	TheLoai		VARCHAR(255),
-	TacGia		VARCHAR(255),
+	TenSach		NVARCHAR(255),
+	TenTheLoai	NVARCHAR(225) FOREIGN KEY REFERENCES THELOAI(TenTheLoai),
+	TacGia		NVARCHAR(255),
 	NamXB		SMALLINT,
-	NhaXB		VARCHAR(255),
+	NhaXB		NVARCHAR(255),
 	NgayNhap	DATE,
 	TriGia		INT,
-	TinhTrang	SMALLINT, -- Lưu số lượng sách còn lại
+	TinhTrang	BIT DEFAULT 1,
 	IsDeleted	BIT DEFAULT 0
 );
 GO
@@ -72,7 +63,6 @@ GO
 
 CREATE TABLE PHIEUTHU
 (
-	--Phiếu thu chỉ được tạo khi sách trả quá hạn, tạo trigger để kiểm tra việc trả quá hạn và tự lập phiếu thu nếu có. Việc truy xuất phiếu thu sẽ được thực hiện thông qua MaPhTra trong bảng PHIEUTHU
 	ID			INT IDENTITY(1, 1) PRIMARY KEY,
 	MaPhTra		VARCHAR(5) FOREIGN KEY REFERENCES PHIEUTRA(MaPhTra),
 	SoNgayQHan	SMALLINT,
@@ -84,25 +74,24 @@ GO
 CREATE TABLE THUTHU
 (
 	MaTT		VARCHAR(5) PRIMARY KEY,
-	HoTen		VARCHAR(30),
-	GioiTinh	VARCHAR(3),
+	HoTen		NVARCHAR(30),
+	GioiTinh	NVARCHAR(3),
 	NgayVLam	DATE,
 	NgaySinh	DATE,
-	DiaChi		VARCHAR(255),
+	DiaChi		NVARCHAR(255),
 	Email		VARCHAR(255) NOT NULL,
 	SoDT		VARCHAR(10) NOT NULL,
-	--AnhDaiDien	VARCHAR(30), -- Nếu cảm thấy không cần thì có thể bỏ qua thuộc tính này (NULL)
 	IsDeleted	BIT DEFAULT 0
 )
+GO
 
 CREATE TABLE ACCOUNT
 (
 	TaiKhoan	VARCHAR(30) PRIMARY KEY,
 	MatKhau		VARCHAR(255) NOT NULL,
-	-- Để phục vụ cho việc thay đổi tên tài khoản, mật khẩu, không nên để tài khoản và mật khẩu làm thuộc tính khóa chính
 	MaDG		VARCHAR(5) FOREIGN KEY REFERENCES DOCGIA(MaDG),
 	MaTT		VARCHAR(5) FOREIGN KEY REFERENCES THUTHU(MaTT),
-	VaiTro		SMALLINT NOT NULL, -- biến này để phân biệt người dùng, thủ thư hay admin, bởi vì mình chỉ sử dụng một bảng này để lưu thông tin tài khoản cho tất cả nên cần có biến này. Mình có thể xét như sau: 3: Admin, 2: thủ thư, và 1: người dùng.
+	VaiTro		SMALLINT NOT NULL,
 	IsDeleted	BIT DEFAULT 0
 );
 GO
@@ -137,8 +126,6 @@ BEGIN
 	UPDATE PHIEUMUON
 	SET NgayPhTra = DATEADD(day, @MAXDAY, NgayMuon)
 	WHERE PHIEUMUON.MaPhMuon = @MaPhMuon
-
-	PRINT('Thông tin phiếu mượn đã được hoàn tất!')
 END;
 GO
 
@@ -164,29 +151,36 @@ BEGIN
 
 		-- Tổng tiền = Số ngày * Tiền nộp 1 ngày
 		INSERT INTO PHIEUTHU VALUES(@MaPhTra, @SoNgQuaHan, @SoNgQuaHan * @IDPhTra, DEFAULT);
-	
-		PRINT('Trả sách quá thời hạn, đã lập phiếu thu!')
 	END
 END;
 GO
 -- INSERT DATA: Dữ liệu để test các chức năng của app
 
 -- DOCGIA TABLE
-INSERT INTO DOCGIA VALUES ('DG001', 'Nguyễn Thanh Hưng', 'LDOCGIA_A', 'Nam', '15/06/2002', '135 Nam Kỳ Khởi Nghĩa, Bến Nghé, Quận 1', 'hungnt@gmail.com', '0392511342', '7/02/2024', '011202534234', DEFAULT);
-INSERT INTO DOCGIA VALUES ('DG002', 'Trần Nguyễn Yến Nhi', 'LDOCGIA_B', 'Nữ', '14/11/2008', 'Đường Lê Lợi, phường Bến Thành, quận 1', 'nhitran08@gmail.com', '0867455258', '7/03/2024', '044208234095', DEFAULT);
-INSERT INTO DOCGIA VALUES ('DG003', 'Trần Lê Tuyết Mai', 'LDOCGIA_A', 'Nữ', '14/10/2004', 'số 2 Nguyễn Bỉnh Khiêm, Quận 1', 'lmaiq1@gmail.com', '0914567842', '12/03/2024', '051204001002', DEFAULT);
-INSERT INTO DOCGIA VALUES ('DG004', 'Lê Thị Ngọc Ánh', 'LDOCGIA_A', 'Nữ', '02/12/2004', 'số 2 Khu Him Lam, quận 7', 'anhngoc@gmail.com', '0392411748', '14/04/2024', '034204543876', DEFAULT);
-INSERT INTO DOCGIA VALUES ('DG005', 'Phan Minh Long', 'LDOCGIA_B', 'Nam', '07/05/2009', 'Số 3 Hòa Bình, phường 3, quận 11', 'longSia@gmail.com', '0974373212', '7/05/2024', '034209112334', DEFAULT);
+INSERT INTO DOCGIA VALUES ('DG001', N'Nguyễn Thanh Hưng', N'Nam', '2002-06-15', N'135 Nam Kỳ Khởi Nghĩa, Bến Nghé, Quận 1', 'hungnt@gmail.com', '0392511342', '2024-02-07', DEFAULT);
+INSERT INTO DOCGIA VALUES ('DG002', N'Trần Nguyễn Yến Nhi', N'Nữ', '2008-11-14', N'Đường Lê Lợi, phường Bến Thành, quận 1', 'nhitran08@gmail.com', '0867455258', '2024-03-07', DEFAULT);
+INSERT INTO DOCGIA VALUES ('DG003', N'Trần Lê Tuyết Mai', N'Nữ', '2004-10-14', N'số 2 Nguyễn Bỉnh Khiêm, Quận 1', 'lmaiq1@gmail.com', '0914567842', '2024-03-12', DEFAULT);
+INSERT INTO DOCGIA VALUES ('DG004', N'Lê Thị Ngọc Ánh', N'Nữ', '2004-12-02', N'số 2 Khu Him Lam, quận 7', 'anhngoc@gmail.com', '0392411748', '2024-04-14', DEFAULT);
+INSERT INTO DOCGIA VALUES ('DG005', N'Phan Minh Long', N'Nam', '2009-05-07', N'Số 3 Hòa Bình, phường 3, quận 11', 'longSia@gmail.com', '0974373212', '2024-05-07', DEFAULT);
+GO
+
+-- THELOAI TABLE
+INSERT INTO THELOAI VALUES (N'Tin học', DEFAULT);
+INSERT INTO THELOAI VALUES (N'Vật lý', DEFAULT);
+INSERT INTO THELOAI VALUES (N'Toán', DEFAULT);
+INSERT INTO THELOAI VALUES (N'Y học', DEFAULT);
+INSERT INTO THELOAI VALUES (N'Văn học', DEFAULT);
+INSERT INTO THELOAI VALUES (N'Lịch sử', DEFAULT);
 GO
 
 -- SACH TABLE
-INSERT INTO  SACH VALUES ('SS001', 'Giáo trình toán cao cấp', 'Giáo trình, toán', 'Nhóm tác giả từ UIT', 2015, 'NXB Đại học quốc gia HCM', '12/6/2020', 60000, 20, DEFAULT);
-INSERT INTO  SACH VALUES ('SS002', 'Giáo trình Hệ điều hành', 'Giáo trình', 'Nhóm tác giả từ UIT', 2015, 'NXB Đại học quốc gia HCM', '12/5/2020', 53000, 18, DEFAULT);
-INSERT INTO  SACH VALUES ('SS003', 'Toán học và ứng dụng', 'Sách tham khảo, toán', 'Nhóm tác giả từ viện toán học Việt Nam', 2014, 'Viện toán học Việt Nam', '12/7/2020', 72000, 3, DEFAULT);
-INSERT INTO  SACH VALUES ('SS004', 'Cẩm nang chăm sóc sức khỏe', 'Sách tham khảo', 'Nguyễn Ngọc Hân, Đặng Huy Hoàng', 2014, 'NXB thông tin và truyền thông', '12/5/2020', 50000, 20, DEFAULT);
-INSERT INTO  SACH VALUES ('SS005', 'Ngôn ngữ lập trình C#', 'Sách tham khảo, tin học', 'Phan Văn Thúc', 2010, 'NXB Giáo dục', '12/5/2020', 54000, 10, DEFAULT);
-INSERT INTO  SACH VALUES ('SS006', 'Đại số tuyến tính', 'Giáo trình, toán', 'Phạm Công Ngô', 2015, 'NXB Đại học quốc gia HCM', '12/6/2020', 65000, 12, DEFAULT);
-INSERT INTO  SACH VALUES ('SS007', 'Vật lý đại cương', 'Giáo trình, vật lý', 'Trần Văn Lượng', 2014, 'NXB Đại học quốc gia HCM', '12/6/2020', 65000, 10, DEFAULT);
+INSERT INTO SACH VALUES ('SS001', N'Giáo trình toán cao cấp', N'Toán', N'Nhóm tác giả từ UIT', 2015, N'NXB Đại học quốc gia HCM', '2020-06-12', 60000, DEFAULT, DEFAULT);
+INSERT INTO SACH VALUES ('SS002', N'Giáo trình Hệ điều hành', N'Tin học', N'Nhóm tác giả từ UIT', 2015, N'NXB Đại học quốc gia HCM', '2020-05-12', 53000, DEFAULT, DEFAULT);
+INSERT INTO SACH VALUES ('SS003', N'Toán học và ứng dụng', N'Toán', N'Nhóm tác giả từ viện toán học Việt Nam', 2014, N'Viện toán học Việt Nam', '2020-07-12', 72000, DEFAULT, DEFAULT);
+INSERT INTO SACH VALUES ('SS004', N'Cẩm nang chăm sóc sức khỏe', N'Y học', N'Nguyễn Ngọc Hân, Đặng Huy Hoàng', 2014, N'NXB thông tin và truyền thông', '2020-05-12', 50000, DEFAULT, DEFAULT);
+INSERT INTO SACH VALUES ('SS005', N'Ngôn ngữ lập trình C#', N'Tin học', N'Phan Văn Thúc', 2010, N'NXB Giáo dục', '2020-05-12', 54000, DEFAULT, DEFAULT);
+INSERT INTO SACH VALUES ('SS006', N'Đại số tuyến tính', N'Toán', N'Phạm Công Ngô', 2015, N'NXB Đại học quốc gia HCM', '2020-06-12', 65000, DEFAULT, DEFAULT);
+INSERT INTO SACH VALUES ('SS007', N'Vật lý đại cương', N'Vật lý', N'Trần Văn Lượng', 2014, N'NXB Đại học quốc gia HCM', '2020-06-12', 65000, DEFAULT, DEFAULT);
 GO
 
 -- PHIEUMUON TABLE
@@ -214,8 +208,8 @@ INSERT INTO PHIEUTRA VALUES ('PT008', 'PM009', '20/05/2024', DEFAULT);
 GO
 
 -- THUTHU TABLE
-INSERT INTO THUTHU VALUES ('TT001', 'Lương Ngọc Huyền', 'Nữ', '5/01/2024', '12/03/2003', 'Phường Bến Thành, Quận 1, Thành phố Hồ Chí Minh', 'huyenlb@gmail.com', '0369442256', DEFAULT);
-INSERT INTO THUTHU VALUES ('TT002', 'Cao Văn Thành', 'Nam', '12/01/2024', '22/05/2003', 'Đường Lê Duẩn, Bến Nghé, Quận 1, Thành phố Hồ Chí Minh', 'thanhcv03@gmail.com', '0973464756', DEFAULT);
+INSERT INTO THUTHU VALUES ('TT001', N'Lương Ngọc Huyền', N'Nữ', '5/01/2024', '12/03/2003', N'Phường Bến Thành, Quận 1, Thành phố Hồ Chí Minh', 'huyenlb@gmail.com', '0369442256', DEFAULT);
+INSERT INTO THUTHU VALUES ('TT002', N'Cao Văn Thành', N'Nam', '12/01/2024', '22/05/2003', N'Đường Lê Duẩn, Bến Nghé, Quận 1, Thành phố Hồ Chí Minh', 'thanhcv03@gmail.com', '0973464756', DEFAULT);
 GO
 
 -- ACOUNT TABLE
