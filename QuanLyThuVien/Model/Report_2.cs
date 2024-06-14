851b1c13851ef6dab6715ebcc0bdbf08dcffa749
Phạm Việt <74823960+HatakaCder@users.noreply.github.com>
2024-06-15 02:37:52 +0700

    temp

---
+++ QuanLyThuVien/Model/Report_2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyThuVien.Model
+{
+    public class Report_2 : INotifyPropertyChanged
+    {
+        #region PropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion 
+
+        private string id;
+
+        public string Id
+        {
+            get { return id; }
+            set { id = value; OnPropertyChanged("Id"); }
+        }
+        private DateTime ngayMuon;
+
+        public DateTime NgayMuon
+        {
+            get { return ngayMuon; }
+            set { ngayMuon = value; OnPropertyChanged("NgayMuon"); }
+        }
+
+
+    }
+}
